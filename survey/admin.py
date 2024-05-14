3dcee916e64161acef8dd366ce89fcb603903e00
Oskop <oskop17@gmail.com>
2024-05-14 12:49:01 +0700

    add all models into admin

--- survey/admin.py
+++ survey/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from survey import models
 # Register your models here.
+
+admin.site.register([
+    models.Instalasi, models.Kamar, models.Kecamatan,
+    models.KelasPelayanan, models.Kelurahan,
+    models.Kotakab, models.Pasien, models.PemakaianKamar,
+    models.Provinsi, models.Registrasi,
+    models.Ruangan, models.SurveiKepuasanMasyarakat,
+    models.TempatTidur])
