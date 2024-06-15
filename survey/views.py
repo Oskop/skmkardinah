8ff5135a0b7c34ef6fa4dc9c1c0108688a1bfab3
Oskop <oskop17@gmail.com>
2024-06-15 08:14:01 +0700

    current survey environment endpoint function

--- survey/views.py
+++ survey/views.py
@@ -2,15 +2,32 @@ from django.shortcuts import render
 from django.http import request
 from django.http import JsonResponse
 
+from survey.controllers.pasiens import get_pasien_by_norm
+from survey.controllers.kamars import get_pemakaiankamar_by
+from survey.controllers.surveys import input_survey
+
 # Create your views here.
 def survey_index(request: request.HttpRequest):
     return render(request=request, template_name="frontend/index.html")
 
 
 def current_survey_environment_by_norm(request: request.HttpRequest):
-    response = {"success": False, "error": "", "data": []}
+    response = {"success": False, "error": "", "data": {}}
     if request.method == 'GET':
-        request.POST.get('norm')
+        if request.POST.get('norm', None):
+            pasien = get_pasien_by_norm(norm=request.POST.get('norm', None))
+            if type(pasien) != "string" and pasien is not None:
+                kamar, error = get_pemakaiankamar_by(pasien)
+                if kamar:
+                    response["data"].update(
+                        {"ruangan":
+                         kamar.id_tempattidur.id_kamar.id_ruangan.nama})
+                    response["data"].update({"NoRM": pasien.nocm})
+                    response["data"].update({"pasien": pasien.nama})
+                    response["data"].update({"noregistrasi":
+                                             kamar.id_registrasi_id})
+            else:
+                response["error"] += f"{pasien}<br>"
     else:
         response['error'] = "method not allowed"
     return JsonResponse(response)
@@ -19,5 +36,11 @@ def current_survey_environment_by_norm(request: request.HttpRequest):
 def survey_input(request: request.HttpRequest):
     response = {"success": "", "error": "", "data": []}
     if request.method == 'POST':
-        request.POST.get('norm')
+        success, message = input_survey(request.POST)
+        if success:
+            response["success"] = True
+        else:
+            response["error"] = message
+    else:
+        response["error"] = "method not allowed"
     return JsonResponse(response)
