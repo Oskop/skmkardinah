ba1a5c2a4cd18a07359a7279330f2f152f1822a5
Oskop <oskop17@gmail.com>
2024-07-13 18:36:13 +0700

    add survey-kirim and survey riwayat pelayanan fix endpoint

--- survey/urls.py
+++ survey/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path
-from survey.views import survey_index, survey_input
+from survey.views import (
+    survey_index, survey_input,
+    current_survey_environment_by_norm)
 from survey.apps import SurveyConfig
 
 urlpatterns = [
-    path("home/", survey_index),
-    path("kirim/", survey_input),
+    path("home/", survey_index, name="survey-home"),
+    path("kirim/", survey_input, name="survey-kirim"),
+    path("kirim/<int:id_registrasi>", survey_input, name="survey-kirim-with-id"),
+    path("riwayatpelayanan/", 
+         current_survey_environment_by_norm,
+         name="survey-riwayat-pelayanan"),
+    path("riwayatpelayanan/<str:norm>", 
+         current_survey_environment_by_norm,
+         name="survey-riwayat-pelayanan-with-norm"),
 ]
