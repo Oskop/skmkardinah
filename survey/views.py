9a8fd6ce6c26d20b3ecb9360d7528c9c6a033297
Oskop <oskop17@gmail.com>
2024-07-13 18:35:31 +0700

    fix all function

--- survey/views.py
+++ survey/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import request
+from django.http.request import HttpRequest
 from django.http import JsonResponse
 
 from survey.controllers.pasiens import get_pasien_by_norm
@@ -7,40 +7,48 @@ from survey.controllers.kamars import get_pemakaiankamar_by
 from survey.controllers.surveys import input_survey
 
 # Create your views here.
-def survey_index(request: request.HttpRequest):
+def survey_index(request: HttpRequest):
     return render(request=request, template_name="frontend/index.html")
 
 
-def current_survey_environment_by_norm(request: request.HttpRequest):
+def current_survey_environment_by_norm(request: HttpRequest, norm: str = None):
     response = {"success": False, "error": "", "data": {}}
-    if request.method == 'GET':
-        if request.POST.get('norm', None):
-            pasien = get_pasien_by_norm(norm=request.POST.get('norm', None))
-            if type(pasien) != "string" and pasien is not None:
+    if norm:
+        if request.method == 'GET':
+            pasien = get_pasien_by_norm(norm=norm)
+            if not isinstance(pasien, str):
                 kamar, error = get_pemakaiankamar_by(pasien)
                 if kamar:
                     response["data"].update(
                         {"ruangan":
-                         kamar.id_tempattidur.id_kamar.id_ruangan.nama})
-                    response["data"].update({"NoRM": pasien.nocm})
+                            kamar.id_tempattidur.id_kamar.id_ruangan.nama})
+                    response["data"].update({"norm": pasien.nocm})
                     response["data"].update({"pasien": pasien.nama})
                     response["data"].update({"noregistrasi":
-                                             kamar.id_registrasi_id})
+                                            kamar.id_registrasi_id})
+                    response["success"] = True
+                else:
+                    response["error"] = error
             else:
-                response["error"] += f"{pasien}<br>"
+                response["error"] = "Pasien tidak ditemukan"
+        else:
+            response['error'] = "Metode tidak diizinkan"
     else:
-        response['error'] = "method not allowed"
+        response["error"] = "NoRM Tidak Terkirim!"
     return JsonResponse(response)
 
 
-def survey_input(request: request.HttpRequest):
-    response = {"success": "", "error": "", "data": []}
-    if request.method == 'POST':
-        success, message = input_survey(request.POST)
-        if success:
-            response["success"] = True
+def survey_input(request: HttpRequest, id_registrasi = None):
+    response = {"success": False, "error": "", "data": []}
+    if id_registrasi != 0:
+        if request.method == 'POST':
+            success, message = input_survey(id_registrasi, request.POST, request.FILES)
+            if success:
+                response["success"] = True
+            else:
+                response["error"] = message
         else:
-            response["error"] = message
+            response["error"] = "Tidak diizinkan"
     else:
-        response["error"] = "method not allowed"
+        response["error"] = "Nomor Registrasi Pasien tidak terkirim"
     return JsonResponse(response)
