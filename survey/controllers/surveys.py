70c1d9cb0a3f7cbf4e7e41437adfeb27fd1d7bdc
Oskop <oskop17@gmail.com>
2024-07-15 01:24:34 +0700

    add get_voice_file, skm_stt, STT_TOKEN, basefilename in handle voice input and fix save voice into komentar_suara

--- survey/controllers/surveys.py
+++ survey/controllers/surveys.py
@@ -1,18 +1,83 @@
 from survey.models import SurveiKepuasanMasyarakat, Registrasi
 from survey.controllers.sherpa import predict_voice_text, ogg_to_wav
+from django.core.files import File
 import pathlib
 from datetime import datetime
+import base64
 
 
-def handle_voice_file(skm: SurveiKepuasanMasyarakat, f):
+STT_TOKEN = '[[stt]]'
+
+
+def get_voice_file(
+        skm: SurveiKepuasanMasyarakat = None,
+        id_skm: int = None, encoded: bool = True):
+    assert skm is not None or id_skm is not None, str(
+        "skm or id_skm parameter must be set!")
+    if skm is None and id_skm is not None:
+        skm = SurveiKepuasanMasyarakat.objects.get(id=id_skm)
+    enc = None
+    parent_dir = 'media/'
+    voice_directory = 'survey/voices/'
+    destination_dir = parent_dir + voice_directory
+    filename = str(f"__{skm.id}__{skm.id_registrasi_id}"
+                   + f"__{skm.id_registrasi.norm_id}")
+    destination_folder = pathlib.Path(destination_dir)
+    if destination_folder.exists():
+        files = list(destination_folder.glob(f'*{filename}*'))
+        try:
+            filename = next(str(f) for f in files if str(f).endswith('.wav'))
+        except StopIteration:
+            try:
+                filename = next(
+                    str(f) for f in files if str(f).endswith('.ogg'))
+            except StopIteration:
+                filename = None
+        if filename and encoded:
+            enc = base64.b64encode(
+                open(filename, "rb").read())
+        elif filename and encoded is False:
+            return filename
+    return enc
+
+
+def skm_stt(skm: SurveiKepuasanMasyarakat = None,
+            id_skm: int = None):
+    assert skm is not None or id_skm is not None, str(
+        "skm or id_skm parameter must be set!")
+    success, message = False, ""
+    if skm is None and id_skm is not None:
+        try:
+            skm = SurveiKepuasanMasyarakat.objects.get(id=id_skm)
+        except SurveiKepuasanMasyarakat.DoesNotExist:
+            message = "Survey tidak ditemukan"
+    if (isinstance(skm.komentar_suara.name, str
+                       ) and skm.komentar_suara.name != ""
+            ) and skm.komentar_suara.name is not None:
+        result, success = predict_voice_text(skm.komentar_suara.path)
+        if success:
+            if len(skm.komentar.split(STT_TOKEN)) > 1:
+                skm.komentar = skm.komentar.split(STT_TOKEN)[0]
+            skm.komentar += f" {STT_TOKEN}{result}{STT_TOKEN}"
+            skm.save()
+            success = True
+        else:
+            message = "perubahan suara ke teks gagal"
+    else:
+        message = "file komentar suara tidak ada"
+    return success, message
+
+
+def handle_voice_file(skm: SurveiKepuasanMasyarakat, f, stt = False):
     message = ""
     parent_dir = 'media/'
     voice_directory = 'survey/voices/'
     destination_dir = parent_dir + voice_directory
     pathlib.Path(destination_dir).mkdir(parents=True, exist_ok=True)
-    filename = str(f"{datetime.now().isoformat().replace(':', '..')}"
-                   + f"__{skm.id}__{skm.id_registrasi_id}"
-                   + f"__{skm.id_registrasi.norm_id}.ogg")
+    basefilename = str(f"{skm.id}__{skm.id_registrasi_id}"
+                   + f"__{skm.id_registrasi.norm_id}")
+    filename = str(f"{datetime.now().isoformat().replace(':', '..')}__"
+                   + basefilename + ".ogg")
     print(str(destination_dir) + filename)
     with open(str(destination_dir) + filename, "wb+") as destination:
         for chunk in f.chunks():
@@ -22,18 +87,24 @@ def handle_voice_file(skm: SurveiKepuasanMasyarakat, f):
     output_wav = filename[:-3] + "wav"
     if ogg_to_wav(str(destination_dir) + filename, 
                   str(destination_dir) + output_wav):
-        result, success = predict_voice_text(
-            str(destination_dir) + output_wav)
-        print(result, success)
-        if success:
-            skm.komentar += f" ;;;[stt]{result}"
-            skm.save()
-            message = f"STT {output_wav} done = {result}"
+        skm.komentar_suara = File(
+            pathlib.Path(str(destination_dir) + output_wav).open(mode='rb'),
+            name=basefilename + '.wav')
+        skm.save()
+        pathlib.Path(str(destination_dir) + output_wav).unlink(missing_ok=True)
+        if skm.komentar_suara.name:
+            message = "file upload successful"
+        if stt:
+            result, success = predict_voice_text(skm.komentar_suara.path)
+            print(result, success)
+            if success:
+                skm.komentar += f" {STT_TOKEN}{result}{STT_TOKEN}"
+                skm.save()
+                message = f"STT {output_wav} done = {result}"
         else:
             message = f"STT {output_wav} failed!"
     else:
         message = f"convert to {output_wav} failed!"
-    print(message)
     return message
 
 
