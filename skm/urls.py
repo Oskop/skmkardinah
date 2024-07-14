a7ecf6b82a135f905221f1255ece4a55d4067e9f
Oskop <oskop17@gmail.com>
2024-07-15 01:22:05 +0700

    add static media url

--- skm/urls.py
+++ skm/urls.py
@@ -18,9 +18,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 from survey.urls import survey_index
+from django.conf.urls.static import static
+from skm import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('survey/', include('survey.urls')),
     path('', survey_index),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT
+           ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
