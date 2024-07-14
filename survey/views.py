5d8b215886cfe6cfc93833f840f11a160272e46a
Oskop <oskop17@gmail.com>
2024-07-15 01:25:13 +0700

    add stt survey view function

--- survey/views.py
+++ survey/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
+from django.contrib import messages
 from django.http.request import HttpRequest
-from django.http import JsonResponse
-
+from django.http import (JsonResponse, HttpResponseRedirect)
 from survey.controllers.pasiens import get_pasien_by_norm
 from survey.controllers.kamars import get_pemakaiankamar_by
-from survey.controllers.surveys import input_survey
+from survey.controllers.surveys import (input_survey, skm_stt)
 
 # Create your views here.
 def survey_index(request: HttpRequest):
@@ -52,3 +52,19 @@ def survey_input(request: HttpRequest, id_registrasi = None):
     else:
         response["error"] = "Nomor Registrasi Pasien tidak terkirim"
     return JsonResponse(response)
+
+
+def stt_survey(request: HttpRequest, id_skm: int = None):
+    response = {"success": False, "error": "", "data": []}
+    if id_skm != 0 and id_skm is not None:
+        if request.method == 'GET':
+            success, message = skm_stt(id_skm=id_skm)
+            if success:
+                messages.success(request, "Berhasil mengubah suara ke teks")
+            else:
+                messages.error(request, message)
+        else:
+            messages.error(request, "Tidak diizinkan")
+    else:
+        messages.warning(request, "Nomor Registrasi Pasien tidak terkirim")
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
