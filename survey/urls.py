ef9ba27ecba69e5f7ae00f67e899e3eda34e123e
Oskop <oskop17@gmail.com>
2024-07-15 01:25:23 +0700

    add stt skm endpoint

--- survey/urls.py
+++ survey/urls.py
@@ -1,13 +1,16 @@
 from django.urls import path
 from survey.views import (
     survey_index, survey_input,
-    current_survey_environment_by_norm)
+    current_survey_environment_by_norm,
+    stt_survey)
 from survey.apps import SurveyConfig
 
 urlpatterns = [
     path("home/", survey_index, name="survey-home"),
     path("kirim/", survey_input, name="survey-kirim"),
     path("kirim/<int:id_registrasi>", survey_input, name="survey-kirim-with-id"),
+    path("stt/", stt_survey, name="survey-stt"),
+    path("stt/<int:id_skm>", stt_survey, name="survey-stt-with-id"),
     path("riwayatpelayanan/", 
          current_survey_environment_by_norm,
          name="survey-riwayat-pelayanan"),
